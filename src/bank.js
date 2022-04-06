6219aec3c9fa285d42a5d2b1d2956c58f706c62e
Chris Hyde <c_hyde333@hotmail.com>
2022-04-06 12:20:11 +0100

    Added Statement Class & Spec

--- src/bank.js
+++ src/bank.js
@@ -1,3 +1,5 @@
+const Statement = require("../src/statement.js");
+
 class Bank {
   constructor() {
     this.transactions = [];
@@ -29,32 +31,36 @@ class Bank {
   }
 
   printStatement() {
-    const statementLines = [];
-    for (let i = 0; i < this.transactions.length; i++) {
-      const transaction = this.transactions[i];
-      if (transaction.type === "credit") {
-        const line = `${transaction.date.replace(
-          /-/g,
-          "/"
-        )} || ${transaction.amount.toFixed(
-          2
-        )} || || ${transaction.balance.toFixed(2)}`;
-        statementLines.push(line);
-      } else {
-        const line = `${transaction.date.replace(
-          /-/g,
-          "/"
-        )} || || ${transaction.amount.toFixed(
-          2
-        )} || ${transaction.balance.toFixed(2)}`;
-        statementLines.push(line);
-      }
-    }
-    const reversedStatementLines = statementLines.reverse();
-
-    reversedStatementLines.unshift("date || credit || debit || balance");
-    return statementLines.join("\n");
+    const statement = new Statement(this.transactions);
+    return statement.print();
   }
+
+  //   const statementLines = [];
+  //   for (let i = 0; i < this.transactions.length; i++) {
+  //     const transaction = this.transactions[i];
+  //     if (transaction.type === "credit") {
+  //       const line = `${transaction.date.replace(
+  //         /-/g,
+  //         "/"
+  //       )} || ${transaction.amount.toFixed(
+  //         2
+  //       )} || || ${transaction.balance.toFixed(2)}`;
+  //       statementLines.push(line);
+  //     } else {
+  //       const line = `${transaction.date.replace(
+  //         /-/g,
+  //         "/"
+  //       )} || || ${transaction.amount.toFixed(
+  //         2
+  //       )} || ${transaction.balance.toFixed(2)}`;
+  //       statementLines.push(line);
+  //     }
+  //   }
+  //   const reversedStatementLines = statementLines.reverse();
+
+  //   reversedStatementLines.unshift("date || credit || debit || balance");
+  //   return statementLines.join("\n");
+  // }
 }
 
 module.exports = Bank;
