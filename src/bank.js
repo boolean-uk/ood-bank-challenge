3f5f89a41f545e73d522634556e1ac97d86b3e8f
Chris Hyde <c_hyde333@hotmail.com>
2022-04-06 10:47:00 +0100

    Print Statement - Done

--- src/bank.js
+++ src/bank.js
@@ -29,14 +29,36 @@ class Bank {
   }
 
   printStatement() {
-    let statementOfTransactions = "";
+    const statementLines = [];
     for (let i = 0; i < this.transactions.length; i++) {
-      // Get the current transaction object which equals this.transactions[i]
-      // Convert properties of transaction into a single string
-      // add the generated string from transaction to statementOfTransactions
+      const transaction = this.transactions[i];
+      if (transaction.type === "credit") {
+        const line = `${transaction.date.replace(
+          /-/g,
+          "/"
+        )} || ${transaction.amount.toFixed(
+          2
+        )} || || ${transaction.balance.toFixed(2)}`;
+        statementLines.push(line);
+      } else {
+        const line = `${transaction.date.replace(
+          /-/g,
+          "/"
+        )} || || ${transaction.amount.toFixed(
+          2
+        )} || ${transaction.balance.toFixed(2)}`;
+        statementLines.push(line);
+      }
     }
-    return statementOfTransactions;
+    const reversedStatementLines = statementLines.reverse();
+
+    reversedStatementLines.unshift("date || credit || debit || balance");
+    return statementLines.join("\n");
   }
 }
 
 module.exports = Bank;
+
+// Get the current transaction object which equals this.transactions[i]
+// Convert properties of transaction into a single string
+// add the generated string from transaction to statementOfTransactions
