ae43eff6ba7aae614c0ca0cce40f262e75a6b931
Pawel Kwiecinski <pawel.kwiecinski@talan.com>
2023-07-24 17:13:07 +0200

    Extension 1

--- src/BankAccount.ts
+++ src/BankAccount.ts
@@ -1,4 +1,5 @@
 import Transaction from "./transaction";
+import transaction from "./transaction";
 
 class BankAccount {
     private transactions: Transaction[] = []
@@ -22,29 +23,45 @@ class BankAccount {
         return true
     }
 
-    public showAccountHistory(): string {
+    public returnAccountHistory(transactions: Transaction[]): string {
         const result: string[] = []
         let balance: number = 0
         result.push("date       || credit  || debit  || balance\n");
 
         this.transactions.forEach((transaction: Transaction) => {
             balance += transaction.amount
+            if(transactions.includes(transaction)) {
+                result.push(transaction.date.toLocaleDateString().padEnd(11) + "||")
 
-            result.push(transaction.date.toLocaleDateString().padEnd(11) + "||")
-
-            if(transaction.amount > 0) {
-                result.push(" " + String(transaction.amount).padEnd(8))
-                result.push("||" + " ".repeat(8))
+                if (transaction.amount > 0) {
+                    result.push(" " + String(transaction.amount).padEnd(8))
+                    result.push("||" + " ".repeat(8))
+                } else {
+                    result.push(" ".repeat(9) + "||")
+                    result.push(" " + String(transaction.amount).padEnd(7))
+                }
+                result.push("|| " + String(balance))
+                result.push("\n")
             }
-            else {
-                result.push(" ".repeat(9) + "||")
-                result.push(" " + String(transaction.amount).padEnd(7))
-            }
-            result.push("|| " + String(balance))
-            result.push("\n")
         })
 
         return result.join("")
+    }
+
+    public showAccountHistory(): string {
+        return this.returnAccountHistory(this.transactions)
+    }
+
+    public showAccountHistoryBetweenTwoDates(date1: Date, date2: Date): string {
+        if(date1 > date2) return "Wrong dates"
+        const transactionBetweenTwoDates: Transaction[] = []
+
+        this.transactions.forEach((transaction: Transaction) => {
+            if(transaction.date > date1 && transaction.date < date2)
+                transactionBetweenTwoDates.push(transaction)
+        })
+
+        return this.returnAccountHistory(transactionBetweenTwoDates)
     }
 }
 
