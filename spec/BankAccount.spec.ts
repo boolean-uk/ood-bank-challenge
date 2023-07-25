7d5d3103830c9e67a4069b95eb2fe747bb63050a
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-25 12:40:33 +0200

    added test for depositing negative amount

--- spec/BankAccount.spec.ts
+++ spec/BankAccount.spec.ts
@@ -8,19 +8,42 @@ describe('BankAccount', () => {
         bankAccount = new BankAccount();
     });
 
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     it('should have a balance of 0', () => {
         expect(bankAccount.balance).toEqual(0);
     });
 
+    it('should have an empty transactions array', () => {
+        expect(bankAccount.transactions).toEqual([]);
+    });
+
     it('should have a balance of 100 when depositing 100', () => {
-        bankAccount.deposit(100);
+        // Mock Date.now() to always return the same time
+        const mockDate = new Date(2022, 7, 25);
+        jest.spyOn(global, 'Date').mockImplementation(() => mockDate as any);
+
         const hour_minute = new Date().getHours() + ":" + new Date().getMinutes();
-        const transaction = new Transaction(100, new Date().toLocaleDateString(), hour_minute);
-        console.log(transaction);
+        const transaction = new Transaction(100, 100, new Date().toLocaleDateString(), hour_minute);
+
+        bankAccount.deposit(100);
+
         expect(bankAccount.balance).toEqual(100);
         expect(bankAccount.transactions).toContainEqual(transaction);
     });
 
+    it('should have a balance of 300 when depositing 100 and 200', () => {
+        bankAccount.deposit(100);
+        bankAccount.deposit(200);
 
+        expect(bankAccount.balance).toEqual(300);
+    });
 
+    it('should not allow a negative deposit', () => {
+        expect(() => {
+            bankAccount.deposit(-100);
+        }).toThrowError('You cannot deposit a negative amount');
+    });
 });
--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,10 +1,12 @@
 class Transaction {
     private _amount: number;
+    private _balance: number;
     private _date: string;
     private _hour_minute: string;
 
-    constructor(amount: number, date: string, hour_minute: string) {
+    constructor(amount: number, balance: number, date: string, hour_minute: string) {
         this._amount = amount;
+        this._balance = balance;
         this._date = date;
         this._hour_minute = hour_minute;
     }
