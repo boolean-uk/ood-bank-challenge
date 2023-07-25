696f025b8ae6ae815f900b32b0c7a103e516da26
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-07-25 13:04:02 +0200

    add tests for BankAccount class

--- docs/spec/BankAccount.spec.ts
+++ docs/spec/BankAccount.spec.ts
@@ -8,5 +8,33 @@ describe("Testing", () => {
         bankAccount = new BankAccount()
     })
 
+    it("should return true", () => {
+        let result: boolean = bankAccount.deposit(100, new Date())
+
+        expect(result).toBe(true)
+    })
+
+    it("negative amount should return false", () => {
+        let result: boolean = bankAccount.deposit(-1000, new Date())
+
+        expect(result).toBe(false);
+    });
+
+    it("should return true", () => {
+        bankAccount.deposit(1000, new Date())
+        let result: boolean = bankAccount.withdraw(1000, new Date())
+
+        expect(result).toBe(true);
+    });
+
+
+    it("withdraw amount too high should return false", () => {
+        bankAccount.deposit(1000, new Date())
+        let result: boolean = bankAccount.withdraw(1100, new Date())
+
+        expect(result).toBe(false);
+    });
+
+
 
 });
