2a4af8086037787f8e57ae1f098b6a5fe57e63ba
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-07-25 13:30:48 +0200

    Extension 3 finished

--- src/Account.ts
+++ src/Account.ts
@@ -2,40 +2,41 @@ import {Transaction} from "./Transaction";
 import {BankStatementGenerator} from "./BankStatementGenerator";
 
 export class Account {
-    private readonly accountOwner: string
-    private readonly transactionHistory: Transaction[]
+    private readonly _accountOwner: string
+    private readonly _transactionHistory: Transaction[]
+    private _allowedOverdraft: number = 0
 
     constructor(accountOwner: string) {
-        this.accountOwner = accountOwner
-        this.transactionHistory = []
+        this._accountOwner = accountOwner
+        this._transactionHistory = []
     }
 
     depositMoney(amountDeposited: number): boolean {
         if (amountDeposited < 0)  {
             return false
         }
-        this.transactionHistory.push(new Transaction(amountDeposited, 'deposit'))
+        this._transactionHistory.push(new Transaction(amountDeposited, 'deposit'))
         return true;
     }
 
     withdrawMoney(amountWithdrawn: number): boolean {
-        if (amountWithdrawn > this.calculateBalance()) {
+        if (amountWithdrawn > this.calculateBalance() + this.allowedOverdraft) {
             return false
         }
-        this.transactionHistory.push(new Transaction(amountWithdrawn, 'withdrawal'))
+        this._transactionHistory.push(new Transaction(amountWithdrawn, 'withdrawal'))
         return true
     }
 
     calculateBalance(): number {
         let balance = 0;
-        for (const transaction of this.transactionHistory) {
+        for (const transaction of this._transactionHistory) {
             balance += transaction.type === 'deposit' ? transaction.amount : -transaction.amount
         }
         return balance
     }
 
     addTransaction(addedTransaction: Transaction) {
-        this.transactionHistory.push(addedTransaction)
+        this._transactionHistory.push(addedTransaction)
     }
 
     generateBankStatement() {
@@ -52,25 +53,37 @@ export class Account {
         return bankStatementGenerator.generateBankStatement(transactions, balance)
     }
 
-    getTransactionHistory(): Transaction[] {
-        return this.transactionHistory
+    get transactionHistory(): Transaction[] {
+        return this._transactionHistory;
+    }
+
+    get allowedOverdraft(): number {
+        return this._allowedOverdraft;
+    }
+
+    allowOverdraft(value: number): boolean {
+        if (value < 0)  {
+            return false
+        }
+        this._allowedOverdraft = value
+        return true
     }
 
     private sortTransactionsOldestToNewest(): Transaction[] {
-        return this.transactionHistory.sort((a, b) => a.transactionDate.getTime() - b.transactionDate.getTime())
+        return this._transactionHistory.sort((a, b) => a.transactionDate.getTime() - b.transactionDate.getTime())
     }
 
     private filterTransactionsByDate(startDate: Date, endDate: Date): Transaction[] {
-        return this.transactionHistory.filter((transaction) => {
+        return this._transactionHistory.filter((transaction) => {
             const transactionDate = transaction.transactionDate;
             return transactionDate >= startDate && transactionDate <= endDate;
         });
     }
 
     private calculateBalanceBeforeTransaction(targetTransaction: Transaction): number {
-        const targetTransactionIndex = this.transactionHistory.indexOf(targetTransaction)
+        const targetTransactionIndex = this._transactionHistory.indexOf(targetTransaction)
 
-        const transactionsBeforeTarget = this.transactionHistory.slice(0, targetTransactionIndex)
+        const transactionsBeforeTarget = this._transactionHistory.slice(0, targetTransactionIndex)
         return transactionsBeforeTarget.reduce((balance, transaction) => {
             balance += transaction.type === 'deposit' ? transaction.amount : -transaction.amount
             return balance
