d87532eaca12ddce94c2d87f0adf4542f02c6cb9
Adam Ciesielski <ciesielski.adam2002@gmail.com>
2023-07-25 11:22:35 +0200

    normal account tests

--- test/bank.test.ts
+++ test/bank.test.ts
@@ -12,5 +12,49 @@ describe("Normal account tests",()=>
         expect(normalAccount.balance).toEqual(0)
         expect(normalAccount.transactions.length).toEqual(0)
     })
+    it("should deposit amount to balance", () =>{
+
+        normalAccount.deposit(100)
+        expect(normalAccount.balance).toEqual(100)
+
+    })
+    it("should not deposit amount to balance", () =>{
+
+      normalAccount.deposit(-100)
+      expect(normalAccount.balance).toEqual(0)
+
+  })
+
+  it("should withdraw from account", () =>{
+
+    normalAccount.deposit(200)
+    normalAcccount.withdraw(100)
+    expect(normalAccount.balance).toEqual(0)
+
+})
+
+it("should not withdraw from account - user wants to withdraw too much", () =>{
+
+  normalAccount.deposit(200)
+  normalAcccount.withdraw(1000)
+  expect(normalAccount.balance).toEqual(0)
+
+})
+
+it("should withdraw from account and deposit", () =>{
+
+  normalAccount.deposit(200)
+  normalAcccount.withdraw(100)
+  expect(normalAccount.balance).toEqual(0)
+
+})
+
+it("should not withdraw from account and deposit - wrong data provided", () =>{
+
+  normalAccount.deposit(200)
+  normalAcccount.withdraw(-100)
+  expect(normalAccount.balance).toEqual(0)
+
+})
 
 })
