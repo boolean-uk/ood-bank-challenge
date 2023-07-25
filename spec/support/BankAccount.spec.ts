4510efcc8b890721b9e77494e9b990c04b1afd01
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-25 11:21:40 +0200

    created red test for initial account balance

---
+++ spec/support/BankAccount.spec.ts
@@ -0,0 +1,15 @@
+import { BankAccount } from '../../src/BankAccount';
+import { Transaction } from '../../src/Transaction';
+
+describe('BankAccount', () => {
+    let bankAccount: BankAccount;
+    
+    beforeEach(() => {
+        bankAccount = new BankAccount();
+    });
+
+    it('should have a balance of 0', () => {
+        expect(bankAccount.balance).toEqual(0);
+    });
+
+});
