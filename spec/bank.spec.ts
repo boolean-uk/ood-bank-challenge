e23e6fe0ac85e4889875dfb946e1cdb6777b4876
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-25 16:58:37 +0200

    Add test for extension: Generate ordered bank statements between 2 dates

--- spec/bank.spec.ts
+++ spec/bank.spec.ts
@@ -26,9 +26,9 @@ describe("Bank account", () => {
     account.deposit(new Decimal(1000), new Date("2012-01-10"));
     account.deposit(new Decimal(2000), new Date("2012-01-13"));
     account.withdraw(new Decimal(500), new Date("2012-01-14"));
+    const statementLines = account.getStatement().printout.split("\n");
 
     // Test
-    const statementLines = account.statement.printout.split("\n");
     expect(statementLines[0].replace(/\s/g, "")).toMatch(/date\|\|credit\|\|debit\|\|balance/);
     expect(statementLines[1].replace(/\s/g, "")).toMatch(/14\/01\/2012\|\|\|\|500.00\|\|2500.00/);
     expect(statementLines[2].replace(/\s/g, "")).toMatch(/13\/01\/2012\|\|2000.00\|\|\|\|3000.00/);
@@ -36,14 +36,32 @@ describe("Bank account", () => {
   });
 
   it("should generate bank statement in pdf", async () => {
+    // Setup
     account.deposit(new Decimal(1000), new Date("2012-01-10"));
     account.deposit(new Decimal(2000), new Date("2012-01-13"));
     account.withdraw(new Decimal(500), new Date("2012-01-14"));
+    const pdfPath = await account.getStatement().downloadPdf();
 
-    const pdfPath = await account.statement.downloadPdf();
-
+    // Test
     fs.access(pdfPath, fs.constants.F_OK, (err) => {
       if (err) throw new Error(`File ${pdfPath} does not exist`);
     });
+  });
+
+  it("should generate ordered bank statement between 2 dates", () => {
+    // Setup
+    account.deposit(new Decimal(1000), new Date("2012-01-10"));
+    account.deposit(new Decimal(2000), new Date("2012-01-13"));
+    account.withdraw(new Decimal(500), new Date("2012-01-14"));
+    account.withdraw(new Decimal(500), new Date("2012-02-01"));
+    const startDate = new Date("2012-01-13");
+    const endDate = new Date("2012-01-14");
+    const statementLines = account.getStatement(startDate, endDate).printout.trim().split("\n");
+
+    // Test
+    expect(statementLines.length).toEqual(3);
+    expect(statementLines[0].replace(/\s/g, "")).toMatch(/date\|\|credit\|\|debit\|\|balance/);
+    expect(statementLines[1].replace(/\s/g, "")).toMatch(/14\/01\/2012\|\|\|\|500.00\|\|2500.00/);
+    expect(statementLines[2].replace(/\s/g, "")).toMatch(/13\/01\/2012\|\|2000.00\|\|\|\|3000.00/);
   });
 });
