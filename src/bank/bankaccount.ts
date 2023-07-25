f2aebbd565eb58b5353310161cefeee689ad1f2c
Rafal Halama <rafau.halama@gmail.com>
2023-07-25 10:24:48 +0200

    deposit implemented

---
+++ src/bank/bankaccount.ts
@@ -0,0 +1,13 @@
+class bankaccount {
+  constructor(private _balance: number = 0) {}
+
+  get balance(): number {
+    return this._balance;
+  }
+
+  deposit(amount: number) {
+    if (amount > 0) this._balance += amount;
+  }
+}
+
+export default bankaccount;
