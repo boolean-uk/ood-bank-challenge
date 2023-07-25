8086dc10ff44da0d8614e40b5a40886be779fed5
Michal Antoniuk <michal.antoniuk@talan.com>
2023-07-25 16:17:48 +0200

    Add test for extension: Generate PDFs of bank statements

--- spec/bank.spec.ts
+++ spec/bank.spec.ts
@@ -1,4 +1,5 @@
 import { Decimal } from "decimal.js";
+import fs from "fs";
 import { Account } from "../src/account";
 import { clearAccounts, openAccount } from "../src/bank";
 
@@ -27,10 +28,22 @@ describe("Bank account", () => {
     account.withdraw(new Decimal(500), new Date("2012-01-14"));
 
     // Test
-    const statementLines = account.statement.split("\n");
+    const statementLines = account.statement.printout.split("\n");
     expect(statementLines[0].replace(/\s/g, "")).toMatch(/date\|\|credit\|\|debit\|\|balance/);
     expect(statementLines[1].replace(/\s/g, "")).toMatch(/14\/01\/2012\|\|\|\|500.00\|\|2500.00/);
     expect(statementLines[2].replace(/\s/g, "")).toMatch(/13\/01\/2012\|\|2000.00\|\|\|\|3000.00/);
     expect(statementLines[3].replace(/\s/g, "")).toMatch(/10\/01\/2012\|\|1000.00\|\|\|\|1000.00/);
+  });
+
+  it("should generate bank statement in pdf", async () => {
+    account.deposit(new Decimal(1000), new Date("2012-01-10"));
+    account.deposit(new Decimal(2000), new Date("2012-01-13"));
+    account.withdraw(new Decimal(500), new Date("2012-01-14"));
+
+    const pdfPath = await account.statement.downloadPdf();
+
+    fs.access(pdfPath, fs.constants.F_OK, (err) => {
+      if (err) throw new Error(`File ${pdfPath} does not exist`);
+    });
   });
 });
