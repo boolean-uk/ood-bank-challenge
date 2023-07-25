6bed53c51b251a43074e0d42c58bc5f32c822d75
Alicja Bobko <alicja.bobko@talan.com>
2023-07-25 15:36:48 +0200

    disable withdraws if insufficient balance test

--- src/__tests__/extension.test.ts
+++ src/__tests__/extension.test.ts
@@ -7,4 +7,21 @@ describe("AccountTest", () => {
   beforeAll(() => {
     customer = new Customer("John", "Doe", new Date("1990-01-01"));
   });
+
+  it("should open a current account with a balance of 0", () => {
+    const current = new CurrentAccount(customer);
+    expect(current.getBalance()).toEqual(0);
+  });
+
+  it("should open a current account with a balance of 1000 calculated from transactions", () => {
+    const current = new CurrentAccount(customer);
+    current.deposit(1000);
+    expect(current.getBalance()).toEqual(1000);
+  });
+
+  it("should throw for insufficient balance", () => {
+    const current = new CurrentAccount(customer);
+    current.deposit(5000);
+    expect(() => current.withdraw(6000)).toThrow("Insufficient funds");
+  });
 });
