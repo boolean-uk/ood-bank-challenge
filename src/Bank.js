4a0ea170b194f03a48f683a0ed13f819e9961dc7
Martyna <martyna.marszalek@talan.com>
2023-07-25 13:11:19 +0200

    creating class bank and implementing functions

--- src/Bank.js
+++ src/Bank.js
@@ -1,15 +1,65 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Bank = void 0;
+exports.Bank = exports.TransactionType = void 0;
 var TransactionType;
 (function (TransactionType) {
     TransactionType["DEPOSIT"] = "DEPOSIT";
     TransactionType["WITHDRAWAL"] = "WITHDRAWAL";
-})(TransactionType || (TransactionType = {}));
+})(TransactionType || (exports.TransactionType = TransactionType = {}));
 var Bank = /** @class */ (function () {
     function Bank() {
+        this.transactions = [];
+        this.balance = 0;
     }
+    Bank.prototype.deposit = function (amount, date) {
+        if (amount <= 0)
+            return false;
+        var transaction = {
+            type: TransactionType.DEPOSIT,
+            amount: amount,
+            date: date,
+        };
+        this.balance += amount;
+        this.transactions.push(transaction);
+        return true;
+    };
+    Bank.prototype.withdraw = function (amount, date) {
+        if (amount <= 0 || this.balance < amount)
+            return false;
+        var transaction = {
+            type: TransactionType.WITHDRAWAL,
+            amount: amount,
+            date: date,
+        };
+        this.balance -= amount;
+        this.transactions.push(transaction);
+        return true;
+    };
+    Bank.prototype.showAccountHistory = function () {
+        var result = "date        || credit    || debit     || balance\n";
+        var balance = 0;
+        for (var _i = 0, _a = this.transactions; _i < _a.length; _i++) {
+            var transaction = _a[_i];
+            var date_1 = transaction.date, amount = transaction.amount, type = transaction.type;
+            var formattedDate = date_1.toLocaleDateString().padEnd(12);
+            var formattedAmount = amount.toFixed(2).padStart(8);
+            if (type === TransactionType.DEPOSIT) {
+                balance += amount;
+                result += "".concat(formattedDate, "|| ").concat(formattedAmount, "  ||           || ").concat(balance.toFixed(2), "\n");
+            }
+            else {
+                balance -= amount;
+                result += "".concat(formattedDate, "||           || ").concat(formattedAmount, "  || ").concat(balance.toFixed(2), "\n");
+            }
+        }
+        return result;
+    };
     return Bank;
 }());
 exports.Bank = Bank;
-module.exports = Bank;
+var bankAccount = new Bank();
+var depositAmount = 1000;
+var date = new Date('2023-07-24');
+var result = bankAccount.deposit(depositAmount, date);
+var accountHistory = bankAccount.showAccountHistory();
+console.log(accountHistory);
--- src/Bank.ts
+++ src/Bank.ts
@@ -10,7 +10,58 @@ interface Transaction {
 }
 
 export class Bank {
+  private transactions: Transaction[] = [];
+  private balance: number = 0;
 
+  public deposit(amount: number, date: Date): boolean {
+    if (amount <= 0) return false;
+
+    const transaction: Transaction = {
+        type: TransactionType.DEPOSIT,
+        amount,
+        date,
+      };
+      this.balance += amount;
+      this.transactions.push(transaction);
+      return true;
+  }
+
+  public withdraw(amount: number, date: Date): boolean {
+    if (amount <= 0 || this.balance < amount) return false;
+
+    const transaction: Transaction = {
+        type: TransactionType.WITHDRAWAL,
+        amount,
+        date,
+      };
+      this.balance -= amount;
+      this.transactions.push(transaction);
+      return true;
+  }
+
+  public showAccountHistory(): string {
+    let result = "date        || credit    || debit     || balance\n";
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      const { date, amount, type } = transaction;
+      const formattedDate = date.toLocaleDateString().padEnd(12);
+      const formattedAmount = amount.toFixed(2).padStart(8);
+
+      if (type === TransactionType.DEPOSIT) {
+        balance += amount;
+        result += `${formattedDate}|| ${formattedAmount}  ||           || ${balance.toFixed(2)}\n`;
+      } else {
+        balance -= amount;
+        result += `${formattedDate}||           || ${formattedAmount}  || ${balance.toFixed(2)}\n`;
+      }
+    }
+
+    return result;
+ }
 }
-
-module.exports = Bank
+const bankAccount = new Bank();
+const depositAmount = 1000;
+const date = new Date('2023-07-24');
+const result = bankAccount.deposit(depositAmount, date);
+const accountHistory = bankAccount.showAccountHistory();
+console.log(accountHistory);
