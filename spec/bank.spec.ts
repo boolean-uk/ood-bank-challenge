dee653f47704c823e9068b16092518a875f5cdea
s21009 <s21009@pjwstk.edu.pl>
2023-07-25 11:41:51 +0200

    test for get Transaction between time period

--- spec/bank.spec.ts
+++ spec/bank.spec.ts
@@ -1,4 +1,5 @@
 import { Bank } from "../bank"
+import { Bank_Transaction } from "../bankTransaction"
 
 describe("Bank tests", () => {
     let bank: Bank
@@ -38,4 +39,27 @@ describe("Bank tests", () => {
         bank.getFunds(fundsToGet);
         expect(bank.getBalance()).toBe(fundsToAdd - fundsToGet);
     })
+
+    it("should return only transactions between chosen time period", () => {
+        const startDate = new Date(2023, 1, 1)
+        const endDate = new Date(2023, 3, 25)
+        const bankTransactionsList : Array<Bank_Transaction> = [
+            new Bank_Transaction(new Date(2022,1,2), "credit", 100),
+            new Bank_Transaction(new Date(2023,1,20), "debit", 50),
+            new Bank_Transaction(new Date(2023,3,24), "credit", 200),
+            new Bank_Transaction(new Date(2023,3,26), "debit", 150),
+            new Bank_Transaction(new Date(2023,2,26), "debit", 150)
+        ]
+
+        bank.bankTransactionsList = bankTransactionsList;
+        const bankTransactionsListByTimePeriod = bank.createTransactionListBetweenTimePeriod(startDate, endDate);
+
+        const expectedTransactions = [
+            new Bank_Transaction(new Date(2023, 1, 20), "debit", 50),
+            new Bank_Transaction(new Date(2023, 3, 24), "credit", 200),
+            new Bank_Transaction(new Date(2023, 2, 26), "debit", 150),
+          ];
+      
+        expect(bankTransactionsListByTimePeriod).toEqual(expectedTransactions);
+    });
 })
