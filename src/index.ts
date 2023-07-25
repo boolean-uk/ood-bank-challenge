8e1644b924353ab8926d0613ad6030bac054956f
Justyna Dudar <justyna.dudar@talan.com>
2023-07-25 12:14:05 +0200

    implementation of the deposit()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
- export class Account{
-    
- }
+export class Account {
+  private _balance: number = 0;
+
+  get balance(): number {
+    return this._balance;
+  }
+
+  deposit(amount: number, date: object) {
+    if (amount > 0) {
+      this._balance += amount;
+      return "The money has been added to your account.";
+    }
+    return "Given amount is invalid.";
+  }
+}
