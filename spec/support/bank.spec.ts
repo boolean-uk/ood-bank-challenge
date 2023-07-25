6b3f7b43b8fcc07606c1ceec87be7ad6405d3e48
Martyna <martyna.marszalek@talan.com>
2023-07-25 13:01:36 +0200

    modyfing test file - the way to present bank operations

--- spec/support/bank.spec.ts
+++ spec/support/bank.spec.ts
@@ -7,14 +7,21 @@ describe("Bank", () => {
     bankAccount = new Bank();
   })
 
+  // it('should deposit money and update the balance', () => {
+  //   const depositAmount = 1000;
+  //   const date = new Date('2023-07-24');
+  //   const result = bankAccount.deposit(depositAmount, date);
+  //   expect(result).toBe(true);
+  //   expect(bankAccount.showAccountHistory()).toContain(`|| ${depositAmount.toFixed(2)}  ||`);
+  //   expect(bankAccount.showAccountHistory()).toContain(`|| ${depositAmount.toFixed(2)}\n`);
+  //   expect(bankAccount.showAccountHistory()).toContain(`|| ${depositAmount.toFixed(2)}  || 1000.00\n`);
+  // });
+
   it('should deposit money and update the balance', () => {
     const depositAmount = 1000;
     const date = new Date('2023-07-24');
     const result = bankAccount.deposit(depositAmount, date);
     expect(result).toBe(true);
-    expect(bankAccount.showAccountHistory()).toContain(`|| ${depositAmount.toFixed(2)}  ||`);
-    expect(bankAccount.showAccountHistory()).toContain(`|| ${depositAmount.toFixed(2)}\n`);
-    expect(bankAccount.showAccountHistory()).toContain(`|| ${depositAmount.toFixed(2)}  || 1000.00\n`);
   });
 
   it('should not deposit negative or zero amount', () => {
@@ -23,8 +30,6 @@ describe("Bank", () => {
     const date = new Date('2023-07-24');
     expect(bankAccount.deposit(negativeAmount, date)).toBe(false);
     expect(bankAccount.deposit(zeroAmount, date)).toBe(false);
-    expect(bankAccount.showAccountHistory()).not.toContain(`|| ${negativeAmount.toFixed(2)}  ||`);
-    expect(bankAccount.showAccountHistory()).not.toContain(`|| ${zeroAmount.toFixed(2)}  ||`);
   });
 
   it('should withdraw money and update the balance', () => {
@@ -33,8 +38,6 @@ describe("Bank", () => {
     const date = new Date('2023-07-25');
     const result = bankAccount.withdraw(withdrawalAmount, date);
     expect(result).toBe(true);
-    expect(bankAccount.showAccountHistory()).toContain(`|| ${withdrawalAmount.toFixed(2)}  ||`);
-    expect(bankAccount.showAccountHistory()).toContain(`||   ${withdrawalAmount.toFixed(2)}  || 1500.00\n`);
   });
 
   it('should not withdraw negative amount or more than the balance', () => {
@@ -44,8 +47,6 @@ describe("Bank", () => {
     const date = new Date('2023-07-25');
     expect(bankAccount.withdraw(negativeAmount, date)).toBe(false);
     expect(bankAccount.withdraw(moreThanBalance, date)).toBe(false);
-    expect(bankAccount.showAccountHistory()).not.toContain(`|| ${negativeAmount.toFixed(2)}  ||`);
-    expect(bankAccount.showAccountHistory()).not.toContain(`||   ${moreThanBalance.toFixed(2)}  ||`);
   });
 
   it('should display the correct account history', () => {
