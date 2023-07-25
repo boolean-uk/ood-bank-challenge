cd5353c29a6f045bb6edefd1a0af3539bba405ad
Rafal Halama <rafau.halama@gmail.com>
2023-07-25 13:40:18 +0200

    savings acc implemented

--- src/bank/SavingsAccount.ts
+++ src/bank/SavingsAccount.ts
@@ -1,8 +1,25 @@
 import BankAccount from "./BankAccount";
+import Transaction from "./Transaction";
 
 class SavingsAccount extends BankAccount {
   constructor(private _depositLimit: number = 20000) {
     super();
+  }
+
+  deposit(amount: number, date: Date) {
+    if (amount > 0) {
+      let currentYear = new Date().getFullYear;
+      let currentYearDepo: number = 0;
+      for (let transaction of this._transactions) {
+        if (transaction.date.getFullYear === currentYear) {
+          currentYearDepo += transaction.amount;
+        }
+      }
+      if (currentYearDepo + amount <= this._depositLimit) {
+        const transaction = new Transaction(date, amount, "credit");
+        this._transactions.push(transaction);
+      }
+    }
   }
 }
 
