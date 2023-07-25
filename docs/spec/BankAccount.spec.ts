9cdb5834560e8f95915a4fa516d2d34009b30ec8
OliwiaKepczynska <oliwia.kepczynska@gmail.com>
2023-07-25 13:18:19 +0200

    implement another methods and tests

--- docs/spec/BankAccount.spec.ts
+++ docs/spec/BankAccount.spec.ts
@@ -34,4 +34,19 @@ describe("Testing", () => {
 
         expect(result).toBe(false);
     });
+
+    it("should return account history", () => {
+        bankAccount.deposit(1000, new Date(2012, 1, 14))
+        bankAccount.deposit(2000, new Date(2012, 1, 13))
+        bankAccount.withdraw(500, new Date(2012, 1, 10))
+
+        let result: string = bankAccount.showAccountHistory()
+        const expected: string[] = []
+        expected.push("date       || credit  || debit  || balance\n");
+        expected.push("2/14/2012  || 1000    ||        || 1000\n");
+        expected.push("2/13/2012  || 2000    ||        || 3000\n");
+        expected.push("2/10/2012  ||         || -500   || 2500\n");
+
+        expect(result).toBe(expected.join(""));
+    });
 });
--- src/BankAccount.ts
+++ src/BankAccount.ts
@@ -25,12 +25,42 @@ class BankAccount {
     }
 
     public withdraw(amount: number, date: Date): boolean {
-        if(amount <= 0) return false
+        if (amount <= 0) return false
 
-        if(this.calculateBalance() + this.overDraft < amount) return false
+        if (this.calculateBalance() + this.overDraft < amount) return false
 
         this.transactions.push({ amount: -amount, date: date })
         return true
+    }
+
+    public returnAccountHistory(transactions: Transaction[]): string {
+        const result: string[] = []
+        let balance: number = 0
+        result.push("date       || credit  || debit  || balance\n");
+
+        this.transactions.forEach((transaction: Transaction) => {
+            balance += transaction.amount
+            if (transactions.includes(transaction)) {
+                let date = (transaction.date.getMonth()+1) + '/' + transaction.date.getDate() + '/' +  transaction.date.getFullYear();
+                result.push(date.padEnd(11) + "||")
+
+                if (transaction.amount > 0) {
+                    result.push(" " + String(transaction.amount).padEnd(8))
+                    result.push("||" + " ".repeat(8))
+                } else {
+                    result.push(" ".repeat(9) + "||")
+                    result.push(" " + String(transaction.amount).padEnd(7))
+                }
+                result.push("|| " + String(balance))
+                result.push("\n")
+            }
+        })
+
+        return result.join("")
+    }
+
+    public showAccountHistory(): string {
+        return this.returnAccountHistory(this.transactions)
     }
 }
 
