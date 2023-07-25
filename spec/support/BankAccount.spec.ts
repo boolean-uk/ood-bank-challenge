bd19cda3bfae7e8575974e75338b2d81300220c4
Kinga <kinga.hernet@talan.com>
2023-07-25 11:56:39 +0200

    Added test for account history

--- spec/support/BankAccount.spec.ts
+++ spec/support/BankAccount.spec.ts
@@ -7,7 +7,7 @@ describe("Testing", () => {
     beforeEach(() => {
         bankAccount = new BankAccount()
     })
-    
+
     it("should return true", () => {
         let result: boolean = bankAccount.deposit(100, new Date())
 
@@ -33,6 +33,21 @@ describe("Testing", () => {
         let result: boolean = bankAccount.withdraw(1100, new Date())
 
         expect(result).toBe(false);
+    });
+
+    it("should return account history", () => {
+        bankAccount.deposit(1000, new Date(2012, 1, 14))
+        bankAccount.deposit(2000, new Date(2012, 1, 13))
+        bankAccount.withdraw(500, new Date(2012, 1, 10))
+
+        let result: string = bankAccount.showAccountHistory()
+        const expected: string[] = []
+        expected.push("date       || credit  || debit  || balance\n");
+        expected.push("2/14/2012  || 1000    ||        || 1000\n");
+        expected.push("2/13/2012  || 2000    ||        || 3000\n");
+        expected.push("2/10/2012  ||         || -500   || 2500\n");
+
+        expect(result).toBe(expected.join(""));
     });
     
 });
