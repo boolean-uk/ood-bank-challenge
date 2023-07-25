79a49640e905b06a17dae45d7f92ac382a0cfa73
s21009 <s21009@pjwstk.edu.pl>
2023-07-25 10:37:23 +0200

    tests for class bank getFunds, getBalance methods

--- spec/bank.spec.ts
+++ spec/bank.spec.ts
@@ -8,12 +8,33 @@ describe("Bank tests", () => {
     })
 
     it("should not add funds, funds number is negatice", () => {
-        expect(bank.addFunds(-100)).toThrowError("Funds number cannot be negative")
+        expect(() => bank.addFunds(-100)).toThrowError("Funds number cannot be negative");
     })
 
     it("should add funds, funds number is positive", () => {
         const fundsToAdd = 100
         bank.addFunds(fundsToAdd);
         expect(bank.getBalance()).toBe(fundsToAdd);
+    })
+
+    it("should balance be 600", () => {
+        const fundsToAdd = 600
+        bank.addFunds(fundsToAdd);
+        expect(bank.getBalance()).toBe(fundsToAdd);
+    })
+
+    it("should balance be 0", () => {
+        expect(bank.getBalance()).toBe(0);
+    })
+
+    it("should not get funds, balance after transaction would be negative", () => {
+        expect(() => bank.getFunds(100)).toThrowError("Balance after transaction would be negative");
+    })
+
+    it("should get funds, balance after transaction would be positive", () => {
+        const fundsToAdd = 100
+        const fundsToGet = 50
+        bank.addFunds(fundsToAdd);
+        expect(bank.getFunds(fundsToGet)).toBe(fundsToAdd - fundsToGet);
     })
 })
