647bcfd5a5b734b89265f56a32f5f094346fe6ae
Martyna <martyna.marszalek@talan.com>
2023-07-25 13:59:44 +0200

    Adding tests for extension 1 (history between two dates)

--- spec/support/bank.spec.ts
+++ spec/support/bank.spec.ts
@@ -57,4 +57,28 @@ describe("Bank", () => {
     expect(bankAccount.showAccountHistory()).toBe(expectedHistory);
 
   });
+  it('should display the correct account history between two dates (transactions not in order)', () => {
+    bankAccount.deposit(1000, new Date('2023-07-24'));
+    bankAccount.deposit(2000, new Date('2023-07-25'));
+    bankAccount.deposit(1500, new Date('2023-07-27'));
+    bankAccount.withdraw(500, new Date('2023-07-26'));
+    bankAccount.withdraw(1000, new Date('2023-07-28'));
+    const startDate = new Date('2023-07-25');
+    const endDate = new Date('2023-07-27');
+    const accountHistory = bankAccount.showAccountHistoryBetweenTwoDates(startDate, endDate);
+    const expectedHistory = `date        || credit    || debit     || balance
+7/25/2023   ||  2000.00  ||           || 3000.00
+7/26/2023   ||           ||   500.00  || 2500.00
+7/27/2023   ||  1500.00  ||           || 4000.00
+`;
+
+    expect(accountHistory).toBe(expectedHistory);
+  });
+
+  it('should return "Wrong dates" when the start date is after the end date', () => {
+    const startDate = new Date('2023-07-28');
+    const endDate = new Date('2023-07-27');
+    const accountHistory = bankAccount.showAccountHistoryBetweenTwoDates(startDate, endDate);
+    expect(accountHistory).toBe('Wrong dates');
+  });
 });
