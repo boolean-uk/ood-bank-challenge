b612e3a6484fc39bf6cc06e75ee38799b8176388
Julia Girejko <julia.girejko@talan.com>
2023-07-25 18:48:07 +0200

    Green stage extensions 3 part 2

--- spec/investmentaccount.spec.ts
+++ spec/investmentaccount.spec.ts
@@ -2,26 +2,27 @@ import { Client } from "../src/client"
 import { Bank } from "../src/bank"
 import { Account } from "../src/account"
 import { SavingAccount } from "../src/savingaccount"
+import { InvestmentAccount } from "../src/investmentaccount"
 
-describe("Saving tests ", () => {
+describe("Investment tests ", () => {
 
     let registerClient: Client
     let newClient: Client
     let bank: Bank
-    let savingAccount: SavingAccount
+    let inAccount: InvestmentAccount
 
     beforeEach(() => { 
         registerClient = new Client("John", "Smith", "01-10-1998")
         newClient = new Client("Chris", "Test", "01-12-1990")
         bank = new Bank()
-        savingAccount = new SavingAccount()
+        inAccount = new InvestmentAccount()
     })
 
     it("should not allowed for overdarft", () => {
         let bankState = 1000
         let amount = 1500
-        savingAccount.deposit(bankState)
-        expect(savingAccount.withdraw(amount)).toBe('Overdraft is disabled in saving account')
+        inAccount.deposit(bankState)
+        expect(inAccount.withdraw(amount)).toBe('Overdraft is disabled in investment account')
     })
 
 })
--- src/investmentaccount.ts
+++ src/investmentaccount.ts
@@ -5,5 +5,21 @@ export class InvestmentAccount extends Account implements PersonalAccount{
     accountType(): string {
         return "Invetment Account"
     }
+
+    withdraw(amount: number, date : string = this.now): string{
+        let tmp = []
+        if(amount > 0){
+            if(amount <= this.countBalanceTotal()){
+                if(!this.debit[date]){
+                    tmp.push(amount)
+                    this.debit[date] = tmp
+                } else {
+                    this.debit[date].push(amount)
+                }
+                return "Money withdrew"
+            }
+            else return "Overdraft is disabled in investment account"
+        } else return "No money to withdraw from deposit"
+    }
     
 }
