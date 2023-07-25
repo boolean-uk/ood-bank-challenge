0a0a0ee63ef7731431732b52a223bf201b05f7dd
Kamil Kulak <kamil.kulak@talan.com>
2023-07-25 15:29:48 +0200

    Added generating bank statements

--- src/Account.ts
+++ src/Account.ts
@@ -46,5 +46,50 @@ export class Account {
 
         return balance
     }
-    
+
+    generateBankStatements() {
+        //   let table = document.querySelector("table");
+        //   let data = Object.keys(this._transactions[0]);
+        //   generateTableHead(table, data);
+        //   generateTable(table, this._transactions);
+
+    //     let document = new Document()
+    //     const body = document.body, table = document.createElement('table')
+
+    //     table.style.width = '100px'
+    //     table.style.border = '1px solid black'
+
+    //     for (let i = 0; i < this.transactions.length; i++) {
+    //         const tr = table.insertRow()
+    //         for (let j = 0; j < 4; j ++) {
+    //             const td = tr.insertCell();
+    //             td.appendChild(document.createTextNode(this.transactions[i].amountOfMoney.toString()))
+    //             td.style.border = '1px solid black';
+    //         }
+    //     }
+
+    // }
+    // Formatowanie nagłówków tabeli
+  const tableHeaders = ['Date', 'Credit', 'Debit', 'Balance'];
+  const headerRow = tableHeaders.map(header => header.padEnd(12)).join('');
+
+  // Formatowanie danych w tabeli
+  let debit: number, credit: number, date, sum = 0 
+  const tableData = this.transactions.map((trasaction) => {
+    date = trasaction.date.toLocaleDateString()
+    if (trasaction instanceof Withdraw) {
+      debit = 0;
+      credit = trasaction.amountOfMoney / 100.0
+      sum -= trasaction.amountOfMoney / 100.0
+    } else if (trasaction instanceof Deposit) {
+      credit = 0;
+      debit = trasaction.amountOfMoney / 100.0
+      sum += trasaction.amountOfMoney / 100.0
+    }
+
+    return `${date.padEnd(12)}${credit.toString().padEnd(12)}${debit.toString().padEnd(12)}${sum.toString()}\n`;
+  });
+
+  return headerRow + '\n' + tableData
+}
 }
