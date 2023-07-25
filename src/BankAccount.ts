c5ccc623635aa19fb82b662f2c1506042b36f440
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-25 12:41:06 +0200

    negative deposit test passes

--- src/BankAccount.ts
+++ src/BankAccount.ts
@@ -13,6 +13,9 @@ class BankAccount {
     }
 
     deposit(amount: number): void {
+        if (amount < 0) {
+            throw new Error('You cannot deposit a negative amount');
+        }
         this._balance += amount;
         const hour_minute = new Date().getHours() + ":" + new Date().getMinutes();
         const transaction = new Transaction(amount, this.balance, new Date().toLocaleDateString(), hour_minute);
