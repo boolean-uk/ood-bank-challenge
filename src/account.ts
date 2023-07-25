da819117b1da18c8a0270eee00f5adf0d77f94e1
Agnieszka Zagorna <a.h.zagorna@gmail.com>
2023-07-25 15:55:13 +0200

    overdraft tests passing

--- src/account.ts
+++ src/account.ts
@@ -4,6 +4,8 @@ import Transaction from "./transaction"
 export class Account {
     private transactions: Transaction[] = []
     private statementGenerator: StatementGenerator = new StatementGenerator()
+    private overdraft: boolean = false
+    private overdraftLimit: number = 500
 
     depositMoney(amount: number) {
         if (amount >= 0) { 
@@ -12,7 +14,9 @@ export class Account {
     }
 
     withdrawMoney(amount: number) {
-        if (amount >= 0 && this.calculateBalance() >= amount) {
+        if (amount >= 0 
+            && ((!this.overdraft && this.calculateBalance() >= amount) 
+            || (this.overdraft && this.calculateBalance() + this.overdraftLimit >= amount))) {
             this.transactions.push(new Transaction(-amount))
         }
     }
@@ -32,5 +36,11 @@ export class Account {
 
     getStatementBetweenDates(date1: Date, date2: Date): string {
         return this.statementGenerator.generateStatementWithDates(this.transactions, date1, date2)
+    }
+
+    addOverdraft() {
+        if(!this.overdraft){
+            this.overdraft = true
+        }
     }
 }
