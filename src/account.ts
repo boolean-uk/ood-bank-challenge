cb025edb2e8b99ad6c60a52858ca6a2ef41583a4
Bartlomiej Walczak <vbawalc@gmail.com>
2023-07-25 19:18:47 +0200

    green stage added

--- src/account.ts
+++ src/account.ts
@@ -8,5 +8,20 @@ class Account {
         this.transactions.push({ amount: amount, date: date })
         return true
     }
+
+    public withdraw(amount: number, date: Date): boolean {
+        if(amount <= 0 || this.getBalance() < amount) 
+        return false
+        this.transactions.push({ amount: -amount, date: date })
+        return true
+    }
+
+    private getBalance(): number {
+        let balance = 0
+        this.transactions.forEach((transaction: Transaction) => {
+            balance += transaction.amount
+        })
+        return balance 
+    }
 }
 export default Account
