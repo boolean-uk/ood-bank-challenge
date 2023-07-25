57b85d803a58ece686209f2326f9a9cd3755292a
Michał Klukowski <michal.klukowski@talan.com>
2023-07-25 12:34:13 +0200

    Add tests for printing statements for date range

--- spec/statement.spec.ts
+++ spec/statement.spec.ts
@@ -17,4 +17,31 @@ describe("Generating bank statements", () => {
 `)
     })
 
+    
+    it("should print bank statement only for 13 and 14 of Januar 2012", () => {
+        const account = new Account("1234")
+        account.deposit(100000, new Date(2012, 0, 10))
+        account.deposit(200000, new Date(2012, 0, 13))
+        account.withdraw(50000, new Date(2012, 0, 14))
+
+        const statement = new BankStatement(account, new Date(2012, 0, 13))
+        expect(statement.print()).toEqual(`date       || credit  || debit  || balance
+14/01/2012 ||         || 500.00 || 2500.00
+13/01/2012 || 2000.00 ||        || 3000.00
+`)
+    })
+
+    it("should print bank statement only for 10 and 13 of Januar 2012", () => {
+        const account = new Account("1234")
+        account.deposit(100000, new Date(2012, 0, 10))
+        account.deposit(200000, new Date(2012, 0, 13))
+        account.withdraw(50000, new Date(2012, 0, 14))
+
+        const statement = new BankStatement(account, new Date(2012, 0, 10), new Date(2012, 0, 13))
+        expect(statement.print()).toEqual(`date       || credit  || debit || balance
+13/01/2012 || 2000.00 ||       || 3000.00
+10/01/2012 || 1000.00 ||       || 1000.00
+`)
+    })
+
 })
