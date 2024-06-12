ac7f1fc148d0251464462b43d2a5aee90a29df6c
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-12 11:25:19 +0200

    add functions and tests to deposit and withdrawl money

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,13 +1,25 @@
-import Bank from "../src/index.js";
+import BankAccount from "../src/index.js";
 
-describe('Bank', () => {
-    let bank
+describe('Bank account', () => {
+    let bankAccount
 
     beforeEach(() => {
-        bank = new Bank()
+        bankAccount = new BankAccount()
     })
 
     it('should exist', () => {
-        expect(bank).toBeInstanceOf(Bank)
+        expect(bankAccount).toBeInstanceOf(BankAccount)
+        expect(bankAccount.balance).toBe(0)
+    })
+
+    it('should be able to deposit money', () => {
+        bankAccount.deposite(1000)
+        expect(bankAccount.balance).toBe(1000)
+    })
+
+    it('should be able to withdraw money', () => {
+        bankAccount.deposite(1000)
+        bankAccount.withdraw(500)
+        expect(bankAccount.balance).toBe(500)
     })
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-class Bank {
+class BankAccount {
+    #transactions
 
+    constructor() {
+        this.#transactions = []
+    }
+
+    get balance() {
+        let balance = 0
+        this.#transactions.forEach((transaction) => {
+            balance += (transaction.credit * 100)
+            balance -= (transaction.debit * 100)
+        })
+
+        return balance / 100
+    }
+
+    deposite(amount) {
+        const date = new Date()
+        this.#transactions.push({date: date, credit: amount, debit: 0})
+    }
+
+    withdraw(amount) {
+        const date = new Date()
+        this.#transactions.push({date: date, credit: 0, debit: amount})
+    }
 }
 
-export default Bank
+export default BankAccount
