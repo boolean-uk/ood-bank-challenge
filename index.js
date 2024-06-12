f276139dc8d04e2b52c14acc2717851646df6e2c
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-06-12 21:44:04 +0100

    working on statement

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ class Bank {
         this.#date = date
         this.#accounts = accounts
         this.#loggedIn = false
-        // console.log(this.#date)
-        // console.log(this.#accounts)
-        // console.log(this.#accounts[0].transactions)
     }
     date(date) {
         if (typeof date !== "string") {
@@ -19,7 +16,6 @@ class Bank {
     }
     logIn(userId, password) {
         const search = this.#accounts.find(account => account.user === userId)
-        // console.log(search)
         if (search === undefined) {
             throw "User doesn't exist"
         }
@@ -33,14 +29,41 @@ class Bank {
         if (this.#loggedIn === false) {
             throw "You must log in"
         }
-        console.log(this.#transactions)
+        if (typeof amount !== "number") {
+            throw "amount must be a number"
+        }
+        this.#transactions.unshift({date: this.#date, type: "credit", amount: amount})
     }
     withdrawl(amount) {
         if (this.#loggedIn === false) {
             throw "You must log in"
         }
+        if (typeof amount !== "number") {
+            throw "amount must be a number"
+        }
+        this.#transactions.unshift({date: this.#date, type: "debit", amount: amount})
     }
     statement() {
+        if (this.#loggedIn === false) {
+            throw "You must log in"
+        }
+        console.log("Date       || Credit    || Debit     || Balance")
+        let total = 0
+        this.#transactions.forEach(transaction => {
+            total += transaction.amount
+            // console.log()
+            let dateText = transaction.date
+            let creditText
+            let debitText
+            // if ()
+            // conditoins for how text is presented
+            // console.log(transaction.date.length)
+            for(let i = dateText.length; i < 11; i++) {
+                dateText += " "
+            }
+
+            console.log(`${dateText}|| 2000.00   || 2000.00   || ${total.toString()}`)
+        })
         
     }
 }
@@ -49,5 +72,5 @@ const bank = new Bank("12/6/2024", [{user: 1, password: "asdf", transactions: [{
 bank.date("12/6/2024")
 bank.logIn(1, "asdf")
 bank.deposit(500)
-
-// console.log("hi there")
+bank.withdrawl(50)
+bank.statement()
