26d23aacc3c52adab84f1d80c660ba3bc52fdfeb
Tim Zoltie <t.zoltie@gmail.com>
2024-06-12 17:10:20 +0100

    account deposit method created

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -4,12 +4,16 @@ import { Account } from "../src/account.js"
 describe('Account', () => {
     let account
     beforeEach(() => {
-        account = new Account()
+        account = new Account('Frank', 'Zappa')
     })
     it('should create an account using their first and last name', () => {
         const result = new Account('Frank', 'Zappa')
         expect(result.firstName).toBe('Frank')
         expect(result.lastName).toBe('Zappa')
         expect(result.credit).toBe(0)
+    })
+    it('should allow an account holder to deposit money into their account', () => {
+        account.deposit(1000)
+        expect(account.credit).toBe(1000)
     })
 })
