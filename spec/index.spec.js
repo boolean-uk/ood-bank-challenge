201f04512b0315519d724154f30fcfe4dc1fc971
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-12 15:15:47 +0200

    add overdraft functions and tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,54 +1,90 @@
-import BankAccount from "../src/index.js";
+import SavingsAccount, { InvestmentAccount, CheckingAccount } from "../src/index.js";
 import currency from "currency.js"
 
 describe('Bank account', () => {
-    let bankAccount
+    let savingsAccount
+    let investmentAccount
+    let checkingAccount
 
     beforeEach(() => {
-        bankAccount = new BankAccount()
+        savingsAccount = new SavingsAccount()
+        checkingAccount = new CheckingAccount()
+        investmentAccount = new InvestmentAccount()
     })
 
     it('should exist', () => {
-        expect(bankAccount).toBeInstanceOf(BankAccount)
-        expect(bankAccount.balance).toBe(0)
+        expect(checkingAccount).toBeInstanceOf(CheckingAccount)
+        expect(checkingAccount.balance).toBe(0)
     })
 
     it('should be able to deposit money', () => {
-        bankAccount.deposite(1000)
+        checkingAccount.deposite(1000)
 
-        expect(bankAccount.balance).toEqual(currency(1000.00))
+        expect(checkingAccount.balance).toEqual(currency(1000.00))
     })
 
     it('should be able to withdraw money', () => {
-        bankAccount.deposite(1000)
-        bankAccount.withdraw(500)
+        checkingAccount.deposite(1000)
+        checkingAccount.withdraw(500)
 
-        expect(bankAccount.balance).toEqual(currency(500.00))
+        expect(checkingAccount.balance).toEqual(currency(500.00))
     })
 
     it('should be able to print a statement', () => {
-        const date = new Date()
-        bankAccount.deposite(1000)
-        bankAccount.deposite(2000)
-        bankAccount.withdraw(500)
+        let date = new Date()
+        date = date.toLocaleDateString()
 
-        const result = bankAccount.getStatement()
+        checkingAccount.deposite(1000)
+        checkingAccount.deposite(2000)
+        checkingAccount.withdraw(500)
 
-        expect(result).toBe(`date || credit || debit || balance\n${date} ||  || 500.00 || 2500.00\n${date} || 2000.00 ||  || 3000.00\n${date} || 1000.00 ||  || 1000.00\n`)
+        const result = checkingAccount.getStatement()
+
+        expect(result).toBe(`date || credit || debit || balance || overdraft\n${date} ||  || 500.00 || 2500.00 || 0\n${date} || 2000.00 ||  || 3000.00 || 0\n${date} || 1000.00 ||  || 1000.00 || 0\n`)
     })
 
     it('should output JSON when getting all transactions', () => {
-        const date = new Date()
-        bankAccount.deposite(1000)
-        bankAccount.deposite(2000)
-        bankAccount.withdraw(500)
+        let date = new Date()
+        date = date.toLocaleDateString()
 
-        expect(bankAccount.transactions).toEqual(JSON.stringify([{date: date, credit: 1000, debit: 0}, {date: date, credit: 2000, debit: 0}, {date: date, credit: 0, debit: 500}]))
+        checkingAccount.deposite(1000)
+        checkingAccount.deposite(2000)
+        checkingAccount.withdraw(500)
+
+        expect(checkingAccount.transactions).toEqual(JSON.stringify([{date: date, credit: 1000, debit: 0}, {date: date, credit: 2000, debit: 0}, {date: date, credit: 0, debit: 500}]))
     })
 
     it('should not be possible to withdrawl more than is in the account', () => {
-        bankAccount.deposite(1000)
+        checkingAccount.deposite(1000)
 
-        expect(() => bankAccount.withdraw(1200)).toThrow('Amount exceeds the available funds')
+        expect(() => checkingAccount.withdraw(1200)).toThrow('Amount exceeds the available funds')
+    })
+
+    it('should be possible to add an overdraft to the checking account', () => {
+        checkingAccount.deposite(1000)
+        checkingAccount.addOverdraft(1000)
+        checkingAccount.withdraw(1200)
+
+        expect(checkingAccount.balance)
+    })
+
+    it('should not be possible to withdrawl more than the overdraft allows', () => {
+        checkingAccount.deposite(1000)
+        checkingAccount.addOverdraft(1000)
+
+        expect(() => checkingAccount.withdraw(2200)).toThrow('Amount exceeds the available funds')
+    })
+
+    it('should be able to print a statement with overdraft', () => {
+        let date = new Date()
+        date = date.toLocaleDateString()
+
+        checkingAccount.deposite(1000)
+        checkingAccount.addOverdraft(1000)
+        checkingAccount.withdraw(1200)
+
+        const result = checkingAccount.getStatement()
+
+        expect(result).toBe(`date || credit || debit || balance || overdraft\n${date} ||  || 1200.00 || -200.00 || 1000.00\n${date} || 1000.00 ||  || 1000.00 || 1000.00\n`)
     })
 })
--- src/index.js
+++ src/index.js
@@ -23,40 +23,56 @@ class BankAccount {
 
     deposite(amount) {
         const depositAmount = currency(amount)
-        const date = new Date()
+        let date = new Date()
+        date = date.toLocaleDateString()
 
         this.#transactions.push({date: date, credit: depositAmount, debit: 0})
     }
 
     withdraw(amount) {
         const withdrawlAmount = currency(amount)
-        const date = new Date()
-
+        let date = new Date()
+        date = date.toLocaleDateString()
         let balance = this.balance
 
-        if(currency(balance).subtract(amount) < 0) {
+        if(currency(balance).add(this.overdraft).subtract(amount) < 0) {
             throw 'Amount exceeds the available funds'
         }
-
 
         this.#transactions.push({date: date, credit: 0, debit: withdrawlAmount})
     }
 
-    getStatement() {
-        const statement = new Statement(this.#transactions)
-        return statement.print()
+    getStatement(date1, date2) {
+        const statement = new Statement(this.#transactions, this.overdraft)
+        return statement.print(date1, date2)
     }
 }
 
 class Statement {
     #transactions
+    #overdraft
 
-    constructor(transactions) {
+    constructor(transactions, overdraft) {
         this.#transactions = transactions
+        this.#overdraft = overdraft
     }
 
-    print() {
+    print(date1, date2) {
+        if(!date1 && !date2) {
+            return this.allStatements()
+        } else {
+            return this.dateStatements(date1, date2)
+        }
+    }
+
+    allStatements() {
+        let overdraft = 0
         let balance = 0
+
+        if (this.#overdraft) {
+            overdraft = this.#overdraft
+        }
+
         function getTransaction(transaction) {
             balance = currency(balance).add(transaction.credit)
             balance = currency(balance).subtract(transaction.debit)
@@ -72,15 +88,71 @@ class Statement {
                 credit = ''
             }
 
-            return `${transaction.date} || ${credit} || ${debit} || ${balance}\n`
+            return `${transaction.date} || ${credit} || ${debit} || ${balance} || ${overdraft}\n`
         }
 
         let allTransactions = this.#transactions.map((transaction) => getTransaction(transaction))
 
         allTransactions = allTransactions.reverse().join('')
 
-        return `date || credit || debit || balance\n${allTransactions}`
+        console.log(`date || credit || debit || balance\n${allTransactions}`)
+
+        return `date || credit || debit || balance || overdraft\n${allTransactions}`
+    }
+
+    dateStatements(date1, date2) {
+
     }
 }
 
-export default BankAccount
+class SavingsAccount extends BankAccount {
+    #type 
+
+    constructor() {
+        super()
+        this.#type = 'savings account'
+    }
+
+    get type() {
+        return this.#type
+    }
+}
+
+class InvestmentAccount extends BankAccount {
+    #type 
+
+    constructor() {
+        super()
+        this.#type = 'investment account'
+    }
+
+    get type() {
+        return this.#type
+    }
+}
+
+class CheckingAccount extends BankAccount {
+    #type 
+    #overdraft
+
+    constructor() {
+        super()
+        this.#type = 'checking account'
+        this.#overdraft = 0
+    }
+
+    get type() {
+        return this.#type
+    }
+
+    addOverdraft(amount) {
+        this.#overdraft = currency(amount)
+    }
+
+    get overdraft() {
+        return this.#overdraft
+    }
+}
+
+export default SavingsAccount
+export { InvestmentAccount, CheckingAccount }
