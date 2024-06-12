509ab34b9f35b68fc67f6dfd03654493f66f3b7a
Farshad <f.bagdeli13@gmail.com>
2024-06-12 13:34:00 +0200

    deposit method tests added

--- spec/bank-account.spec.js
+++ spec/bank-account.spec.js
@@ -1,5 +1,5 @@
 import { BankAccount } from "../src/bank-account.js"
-
+import { Transaction } from "../src/transaction.js"
 describe('BankAccount', () => {
   let myBankAccount
 
@@ -9,5 +9,13 @@ describe('BankAccount', () => {
 
   it('should be instance of Bank Account', () => {
     expect(myBankAccount).toBeInstanceOf(BankAccount)
+  })
+
+  it('should balance be 2500', () => {
+    myBankAccount.deposit(1000, '10-01-2012', 'debit')
+    myBankAccount.deposit(500, '11-01-2012', 'debit')
+    myBankAccount.deposit(1000, '12-01-2012', 'debit')
+    const balance = myBankAccount.getBalance
+    expect(balance).toBe(2500)
   })
 })
--- src/bank-account.js
+++ src/bank-account.js
@@ -1,9 +1,23 @@
+import { Transaction } from "./transaction.js"
+
 class BankAccount {
   constructor() {
     this.transaction = []
     this.balance = 0
   }
 
+  deposit(amount, date, type) {
+    this.balance += amount
+    this.transaction = new Transaction(amount, date, type, this.balance)
+  }
+
+  get getTransactions() {
+    return this.transaction
+  }
+
+  get getBalance() {
+    return this.balance
+  }
   
 }
 
