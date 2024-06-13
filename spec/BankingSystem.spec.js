d288f904e30e74677915116bdaa02d338d071518
Leonardo Lodi <your_email@address>
2024-06-13 01:04:50 +0100

    add print bank statement between dates method and test it

--- spec/BankingSystem.spec.js
+++ spec/BankingSystem.spec.js
@@ -7,58 +7,82 @@ describe('Banking System', () => {
         client = new BankingSystem()
     })
 
-    it('should make a deposite', () => {
-        let expected = `date                 || `
-        expected += `credit               || `
-        expected += `debit                || `
-        expected += `balance             \n`
-        expected += `Wed Jun 12 2024      || `
-        expected += `50.00                || `
-        expected += `                     || `
-        expected += `50.00               \n`
-        client.deposit(50)
+    it('should make a deposit', () => {
+        let expected = `date            || `
+        expected += `credit          || `
+        expected += `debit           || `
+        expected += `balance        \n`
+        expected += `Fri Jun 14 2024 || `
+        expected += `50.00           || `
+        expected += `                || `
+        expected += `50.00          \n`
+        client.deposit('2024-6-14', 50)
         expect(client.printBankStatement()).toEqual(expected)
     })
 
     it('should make a withdraw', () => {
-        let expected = `date                 || `
-        expected += `credit               || `
-        expected += `debit                || `
-        expected += `balance             \n`
-        expected += `Wed Jun 12 2024      || `
-        expected += `                     || `
-        expected += `25.00                || `
-        expected += `25.00               \n`
-        expected += `Wed Jun 12 2024      || `
-        expected += `50.00                || `
-        expected += `                     || `
-        expected += `50.00               \n`
-        client.deposit(50)
-        client.withdraw(25)
+        let expected = `date            || `
+        expected += `credit          || `
+        expected += `debit           || `
+        expected += `balance        \n`
+        expected += `Sat Jun 15 2024 || `
+        expected += `                || `
+        expected += `25.00           || `
+        expected += `25.00          \n`
+        expected += `Fri Jun 14 2024 || `
+        expected += `50.00           || `
+        expected += `                || `
+        expected += `50.00          \n`
+        client.deposit('2024-6-14', 50)
+        client.withdraw('2024-6-15', 25)
         expect(client.printBankStatement()).toEqual(expected)
     })
 
     it('should print bank statement', () => {
-        let expected = `date                 || `
-        expected += `credit               || `
-        expected += `debit                || `
-        expected += `balance             \n`
-        expected += `Wed Jun 12 2024      || `
-        expected += `100.00               || `
-        expected += `                     || `
-        expected += `125.00              \n`
-        expected += `Wed Jun 12 2024      || `
-        expected += `                     || `
-        expected += `25.00                || `
-        expected += `25.00               \n`
-        expected += `Wed Jun 12 2024      || `
-        expected += `50.00                || `
-        expected += `                     || `
-        expected += `50.00               \n`
-        client.deposit(50)
-        client.withdraw(25)
-        client.deposit(100)
+        let expected = `date            || `
+        expected += `credit          || `
+        expected += `debit           || `
+        expected += `balance        \n`
+        expected += `Sun Jun 16 2024 || `
+        expected += `100.00          || `
+        expected += `                || `
+        expected += `125.00         \n`
+        expected += `Sat Jun 15 2024 || `
+        expected += `                || `
+        expected += `25.00           || `
+        expected += `25.00          \n`
+        expected += `Fri Jun 14 2024 || `
+        expected += `50.00           || `
+        expected += `                || `
+        expected += `50.00          \n`
+        client.deposit('2024-6-14', 50)
+        client.withdraw('2024-6-15', 25)
+        client.deposit('2024-6-16', 100)
         expect(client.printBankStatement()).toEqual(expected)
+    })
+
+    it('should print bank statement between dates', () => {
+        let expected = `date            || `
+        expected += `credit          || `
+        expected += `debit           || `
+        expected += `balance        \n`
+        expected += `Mon Jul 15 2024 || `
+        expected += `                || `
+        expected += `25.00           || `
+        expected += `75.00          \n`
+        expected += `Wed Jun 26 2024 || `
+        expected += `50.00           || `
+        expected += `                || `
+        expected += `100.00         \n`
+        expected += `Fri Jun 14 2024 || `
+        expected += `50.00           || `
+        expected += `                || `
+        expected += `50.00          \n`
+        client.deposit('2024-6-14', 50)
+        client.deposit('2024-6-26', 50)
+        client.withdraw('2024-7-15', 25)
+        client.deposit('2024-8-16', 50)
+        expect(client.printBankStatementBetweenDates('2024-6-14', '2024-7-15')).toEqual(expected)
         console.log(`\n${expected}`)
     })
 })
--- src/BankingSystem.js
+++ src/BankingSystem.js
@@ -1,38 +1,57 @@
 export default class BankingSystem {
-    #date
     #credit
     #debit
     #balance
     #transaction
+    #lastTransactionDate
 
     constructor() {
-        this.#date = new Date().toDateString()
         this.#credit = 0
         this.#debit = 0
         this.#balance = 0
         this.#transaction = []
+        this.#lastTransactionDate = new Date().setHours(0, 0, 0, 0)
     }
 
     #isNumber(num) {
         return typeof num === 'number'
     }
 
-    deposit(amount) {
-        if (!this.#isNumber(amount) || amount <= 0) {
+    #isValidDate(date) {
+        return new Date(date) >= this.#lastTransactionDate
+    }
+
+    deposit(date, amount) {
+        if (
+            !this.#isNumber(amount) || 
+            !this.#isValidDate(date) || 
+            amount <= 0
+        ) {
             throw new Error('Deposit invalidated.')
         }
 
         this.#balance += amount
-        this.#transaction.push([this.#date, amount.toFixed(2), '', this.#balance.toFixed(2)])
+        this.#transaction.push(
+            [new Date(date).toDateString(), amount.toFixed(2), '', this.#balance.toFixed(2)]
+        )
+        this.#lastTransactionDate = new Date(date)
     }
 
-    withdraw(amount) {
-        if (!this.#isNumber(amount) || amount > this.#balance || amount <= 0) {
+    withdraw(date, amount) {
+        if (
+            !this.#isNumber(amount) || 
+            !this.#isValidDate(date) || 
+            amount > this.#balance || 
+            amount <= 0
+        ) {
             throw new Error('Withdraw invalidated.')
         }
         
         this.#balance -= amount
-        this.#transaction.push([this.#date, '', amount.toFixed(2), this.#balance.toFixed(2)])
+        this.#transaction.push(
+            [new Date(date).toDateString(), '', amount.toFixed(2), this.#balance.toFixed(2)]
+        )
+        this.#lastTransactionDate = new Date(date)
     }
 
     #givePad(name, length) {
@@ -41,10 +60,42 @@ export default class BankingSystem {
     }
 
     printBankStatement() {
-        let bankStatement = `${this.#givePad('date', 20)} || ${this.#givePad('credit', 20)} || ${this.#givePad('debit', 20)} || ${this.#givePad('balance', 20)}\n`
+        if (this.#transaction.length === 0) {
+            throw new Error('No transition found.')
+        }
+
+        let bankStatement = `${this.#givePad('date', 15)} || ${this.#givePad('credit', 15)} || ${this.#givePad('debit', 15)} || ${this.#givePad('balance', 15)}\n`
 
         this.#transaction.reverse().forEach(t => {
-            bankStatement += `${this.#givePad(t[0], 20)} || ${this.#givePad(t[1], 20)} || ${this.#givePad(t[2], 20)} || ${this.#givePad(t[3], 20)}\n`
+            bankStatement += `${this.#givePad(t[0], 15)} || ${this.#givePad(t[1], 15)} || ${this.#givePad(t[2], 15)} || ${this.#givePad(t[3], 15)}\n`
+        })
+
+        return bankStatement
+    }
+
+    printBankStatementBetweenDates(startDate, endDate) {
+        if (new Date(endDate) <= new Date(startDate)) {
+            throw new Error('Bank Statement print invalidated.')
+        }
+        
+        let transactions = []
+        let bankStatement = `${this.#givePad('date', 15)} || ${this.#givePad('credit', 15)} || ${this.#givePad('debit', 15)} || ${this.#givePad('balance', 15)}\n`
+
+        this.#transaction.filter(t => {
+            if (
+                new Date(t[0]).valueOf() >= new Date(startDate).valueOf() && 
+                new Date(t[0]).valueOf() <= new Date(endDate).valueOf()
+            ) {
+                transactions.push(t)
+            }
+        })
+        
+        if (transactions.length === 0) {
+            throw new Error('No transaction found.')
+        }
+
+        transactions.reverse().forEach(t => {
+            bankStatement += `${this.#givePad(t[0], 15)} || ${this.#givePad(t[1], 15)} || ${this.#givePad(t[2], 15)} || ${this.#givePad(t[3], 15)}\n`
         })
 
         return bankStatement
