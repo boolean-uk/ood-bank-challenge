9e2038f65a273cfa7058db8cc39cb457a650f6ef
homonoviscoding <mama.leye@gmail.com>
2024-06-20 05:26:54 +0100

    core assignment complete

--- spec/bank-challenge.spec.js
+++ spec/bank-challenge.spec.js
@@ -118,7 +118,10 @@ const {Bank, Account, DebitTransaction, CreditTransaction, Transaction} = requir
           bank.printStatement(accountNumber)
     
 
+          expect(spyConsole).toHaveBeenCalledTimes(5)
           expect(spyConsole.calls.argsFor(0)[0]).toContain('date\t\t|| credit\t|| debit\t|| balance')
+          expect(spyConsole.calls.argsFor(1)[0]).toContain('10/01/2012\t\t|| £10.00\t|| \t\t|| £10.00')
+          expect(spyConsole.calls.argsFor(2)[0]).toContain('14/01/2012\t\t|| \t\t|| £5.00\t|| £5.00')
         })
     
         it('should log an error message when the account does not exist', () => {
@@ -173,16 +176,101 @@ const {Bank, Account, DebitTransaction, CreditTransaction, Transaction} = requir
       
         describe('getTransactions', () => {
           it('should return the list of transactions for the account', () => {
-            expect(account.getTransactions().length).toEqual(0);
+            expect(account.getTransactions().length).toEqual(0)
       
-            account.addTransaction(new CreditTransaction(new Date(), 100));
-            account.addTransaction(new DebitTransaction(new Date(), 50));
+            account.addTransaction(new CreditTransaction(new Date(), 100))
+            account.addTransaction(new DebitTransaction(new Date(), 50))
       
-            expect(account.getTransactions().length).toEqual(2);
-          });
-        });
-    });
-})
-  
+            expect(account.getTransactions().length).toEqual(2)
+          })
+        })
+    })
 
+    describe('Transaction', () => {
+        let transaction
+        let date
+        let amount
+      
+        beforeEach(() => {
+          date = new Date()
+          amount = 100
+          transaction = new Transaction(date, amount)
+        })
+      
+        describe('getAmount', () => {
+          it('should return the amount of the transaction', () => {
+            expect(transaction.getAmount()).toEqual(amount)
+          })
+        })
+      
+        describe('formatDate', () => {
+          it('should format the date correctly as "dd/mm/yyyy"', () => {
+            const formattedDate = transaction.formatDate()
+            const expectedFormat = `${date.getDate()}/0${date.getMonth() + 1}/${date.getFullYear()}`
+      
+            expect(formattedDate).toEqual(expectedFormat)
+          })
+      
+          it('should return an empty string if the date is not an instance of Date', () => {
+            transaction.date = null
+      
+            expect(transaction.formatDate()).toEqual('')
+          })
+        })
+      })
+      
+      describe('CreditTransaction', () => {
+        let creditTransaction
+        let date
+        let amount
+      
+        beforeEach(() => {
+          date = new Date()
+          amount = 100
+          creditTransaction = new CreditTransaction(date, amount)
+        })
+      
+        describe('credit', () => {
+          it('should return the amount as a formatted credit string', () => {
+            const formattedCredit = new Intl.NumberFormat(undefined, {
+              style: 'currency',
+              currency: 'GBP',
+            }).format(amount)
+      
+            expect(creditTransaction.credit).toEqual(formattedCredit)
+          })
+      
+          it('should return undefined for the debit property', () => {
+            expect(creditTransaction.debit).toBeUndefined()
+          })
+        })
+      })
+      
+      describe('DebitTransaction', () => {
+        let debitTransaction
+        let date
+        let amount
+      
+        beforeEach(() => {
+          date = new Date()
+          amount = 100
+          debitTransaction = new DebitTransaction(date, amount)
+        })
+      
+        describe('debit', () => {
+          it('should return the amount as a formatted debit string', () => {
+            const formattedDebit = new Intl.NumberFormat(undefined, {
+              style: 'currency',
+              currency: 'GBP',
+            }).format(amount)
+      
+            expect(debitTransaction.debit).toEqual(formattedDebit)
+          })
+      
+          it('should return undefined for the credit property', () => {
+            expect(debitTransaction.credit).toBeUndefined()
+          })
+        })
+    })
+})
   
